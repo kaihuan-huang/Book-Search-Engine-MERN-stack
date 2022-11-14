3b621045438ceace73a9973be08ef38c3eac5089
kaihuan Huang <huanhuan@kaihuans-MacBook-Pro.local>
2022-11-14 12:40:05 -0800

    Export your typeDefs and resolvers

---
+++ server/Schemas/index.js
@@ -0,0 +1,5 @@
+//Export your typeDefs and resolvers.
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,1 @@
+//Define the query and mutation functionality to work with the Mongoose models.
