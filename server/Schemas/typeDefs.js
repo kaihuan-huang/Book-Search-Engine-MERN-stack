51c00062399469aa2639197e37d0ee8c16ee0dd9
kaihuan Huang <huanhuan@kaihuans-MacBook-Pro.local>
2022-11-15 14:59:51 -0800

    Define the query and mutation functionality to work with the Mongoose models.

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    description: String
+    title: String
+    image: String
+    link: String
+    authors: [String]!
+  }
+   
+  input NewBook {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+ 
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth 
+    savedBook(newBook: NewBook ): User
+    removeBook(bookId: ID!): User
+   
+  }
+`;
+
+module.exports = typeDefs;
