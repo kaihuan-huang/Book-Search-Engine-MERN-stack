8936fd4fa88da42bfe1fd433b2086b68e6bc214f
kaihuan Huang <huanhuan@kaihuans-MacBook-Pro.local>
2022-11-21 21:28:05 -0800

    update heroku

--- server/Schema/typeDefs.js
+++ server/Schema/typeDefs.js
@@ -40,7 +40,7 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth 
-    savedBook(newBook: newBook ): User
+    saveBook(newBook: newBook ): User
     removeBook(bookId: ID!): User
    
   }
